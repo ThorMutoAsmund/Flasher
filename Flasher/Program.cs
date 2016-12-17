895c74f5ce66760e60320740bb02e69deef4a008
TMA <tma@ida.dk>
2016-12-17 19:41:36 +0100

    First checkin

---
+++ Flasher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("USAGE: flasher.exe <flashcardfile>");
+                return;
+            }
+
+            var flashCardFile = args[0];
+            if (!File.Exists(flashCardFile))
+            {
+                Console.WriteLine($"File not found '{flashCardFile}'");
+                return;
+            }
+
+            var cards = File.ReadAllLines(flashCardFile).Select(l => l.Trim()).Where(l => !String.IsNullOrEmpty(l) && !l.StartsWith("#")).ToArray();
+            if (cards.Length == 0)
+            {
+                Console.WriteLine("No cards in file. Each card must be placed on its own line.");
+                return;
+            }
+
+            Console.WriteLine($"{cards.Length} cards found. Press enter to start, then enter to get a new card. Press 'q' to quit.");
+            Console.ReadKey();
+
+            var rand = new Random(DateTime.Now.Millisecond);
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(cards[rand.Next(cards.Length)]);
+            }
+            while (Console.ReadKey().KeyChar != 'q');
+        }
+    }
+}
---
+++ Flasher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Flasher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IDA")]
+[assembly: AssemblyProduct("Flasher")]
+[assembly: AssemblyCopyright("Copyright © IDA 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("def9f707-9362-4c20-be89-ed3b8178a096")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
